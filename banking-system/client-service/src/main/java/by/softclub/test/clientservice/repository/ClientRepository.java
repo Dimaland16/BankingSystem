dcbe2995255bd74dcc7e1db4505c95aff5146b29
Dimaland16 <hello.dm.lol@gmail.com>
2025-04-14 11:45:29 +0300

    feat: implement client search by multiple parameters using Specification

--- banking-system/client-service/src/main/java/by/softclub/test/clientservice/repository/ClientRepository.java
+++ banking-system/client-service/src/main/java/by/softclub/test/clientservice/repository/ClientRepository.java
@@ -2,12 +2,12 @@ package by.softclub.test.clientservice.repository;
 
 import by.softclub.test.clientservice.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends JpaRepository<Client, Long>, JpaSpecificationExecutor<Client> {
 
     Boolean existsByContactInfoEmail(String email);
     Boolean existsByContactInfoPhoneNumber(String phoneNumber);
